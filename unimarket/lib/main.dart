f87891a911780c52a51cafeddb8cdd423312521c
lauralara13021 <lv.larad@uniandes.edu.co>
2025-04-16 20:48:43 -0500

    Initialized Hive and Firebase configurations for saving finds offline

--- unimarket/lib/main.dart
+++ unimarket/lib/main.dart
@@ -17,7 +17,8 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
+  await Hive.initFlutter();
+  await HiveFindStorage.initialize();
  
   await HiveChatStorage.initialize();
 
