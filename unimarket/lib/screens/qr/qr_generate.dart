31390f0845cc40b3633b4b90541eef4533cc09de
MATEO_LOPEZ <m.lopez112@uniandes.edu.co>
2025-04-17 23:48:51 -0500

    QR generator cache

--- unimarket/lib/screens/qr/qr_generate.dart
+++ unimarket/lib/screens/qr/qr_generate.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:pretty_qr_code/pretty_qr_code.dart';
 import 'package:unimarket/data/firebase_dao.dart';
 import 'package:unimarket/screens/ble_scan/seller_ble_advertiser.dart';
-
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'dart:convert';
+import 'dart:typed_data';
 
 class QrGenerate extends StatefulWidget {
   const QrGenerate({super.key});
@@ -16,6 +18,9 @@ class _QrGenerateState extends State<QrGenerate> {
   //Aplica el patrón de DAO
   final FirebaseDAO _firebaseDAO = FirebaseDAO(); 
   final SellerBLEAdvertiser _sellerBLEAdvertiser = SellerBLEAdvertiser();
+  //Vaina nueva para poder usar el cache
+  final cache = DefaultCacheManager();
+  
   Map<String, Map<String, dynamic>>? _productsWithHashes;
   bool _isLoading = true;
 
@@ -26,19 +31,49 @@ class _QrGenerateState extends State<QrGenerate> {
   }
 
   Future<void> _fetchProducts() async {
-    try {
-      final productsWithHashes = await _firebaseDAO.getProductsForCurrentSELLER();
+  const llaveCache = 'qr_productos';
+  try {
+    final fileInfo = await cache.getFileFromCache(llaveCache);
+    if (fileInfo != null) {
+      print("Fetched products successfully from cache!");
+      //Como el Cache Manager administra todo como archivos JSON, 
+      //Es necesario hacerle transformaciones
+      final file = fileInfo.file;
+      final jsonStr = await file.readAsString();
+      final decoded = json.decode(jsonStr) as Map<String, dynamic>;
+      //Paso final, debería funcionar igual
+      final cachedMap = decoded.map((key, value) =>
+        MapEntry(key, Map<String, dynamic>.from(value as Map)));
+
       setState(() {
-        _productsWithHashes = productsWithHashes;
+        _productsWithHashes = cachedMap;
         _isLoading = false;
       });
-    } catch (e) {
-      print("Error fetching products: $e");
-      setState(() {
-        _isLoading = false;
-      });
+
+      print("Loaded products from cache");
+      return;
     }
+
+    // Si no funciona el cache, ir por la de firebase
+    final productsWithHashes = await _firebaseDAO.getProductsForCurrentSELLER();
+    final jsonString = json.encode(productsWithHashes);
+    final bytes = Uint8List.fromList(utf8.encode(jsonString));
+    //Se manda a cache el archivo json creado, debería funcionar ojala
+    await cache.putFile(llaveCache, bytes);
+
+    setState(() {
+      _productsWithHashes = productsWithHashes;
+      _isLoading = false;
+    });
+
+    print("Fetched products from firebase, successfully cached them all");
+  } catch (e) {
+    print("Error fetching products: $e");
+    setState(() {
+      _isLoading = false;
+    });
   }
+}
 
   void _showQrPopup(String hashConfirm) {
     
